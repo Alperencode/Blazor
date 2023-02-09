fe2b9b345fe1a7ea4587d22ca67c2dc56ddf40d6
Alperen Aga <alperencode@gmail.com>
2023-02-09 06:01:22 +0300

    Finish the AddressApp basic structure Will add JSON integration (API)

---
+++ AddressApp/AddressApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace AddressApp.Models
+{
+    public class User
+    {
+        public static int counter = 0;
+
+        public int ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Phone { get; set; }
+    }
+}
